d7d6b4327817d9ef3a3ff7c71c4073b5b5656c4e
Nickle Wang <nickle.wang@hpe.com>
2022-03-03 16:14:48 +0800

    Use RedfishBase.h

--- RedfishClientPkg/Library/RedfishFeatureUtilityLib/RedfishFeatureUtilityInternal.h
+++ RedfishClientPkg/Library/RedfishFeatureUtilityLib/RedfishFeatureUtilityInternal.h
@@ -1,7 +1,7 @@
 /** @file
   Common header file for RedfishFeatureUtilityLib driver.
 
-  (C) Copyright 2020-2021 Hewlett Packard Enterprise Development LP<BR>
+  (C) Copyright 2020-2022 Hewlett Packard Enterprise Development LP<BR>
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -11,6 +11,7 @@
 #define REDFISH_FEATURE_INTERNAL_H_
 
 #include <Uefi.h>
+#include <RedfishBase.h>
 
 #include <Library/BaseLib.h>
 #include <Library/BaseMemoryLib.h>
@@ -32,14 +33,12 @@
 
 #define INDEX_VARIABLE_SIZE       64
 #define INDEX_STRING_SIZE         16
-#define IS_EMPTY_STRING(a)        (a == NULL || a[0] == '\0')
 #define INDEX_STRING              L"{%d}"
 #define SCHEMA_NAME_PREFIX_OFFSET 15 // x-uefi-redfish-
 #define REDFISH_SYSTEM_ROOT_PATH  "/v1/Systems"
 #define REDFISH_ROOT_PATH         "/v1"
 #define MAX_CONF_LANG_LEN         128
 #define SYSTEM_ID_VARIABLE_NAME   L"RedfishSystemId"
-#define REDFISH_DEBUG_TRACE       DEBUG_INFO
 
 #define BIOS_CONFIG_TO_REDFISH_REDPATH_ARRAY_START_SIGNATURE L"{"
 #define BIOS_CONFIG_TO_REDFISH_REDPATH_ARRAY_END_SIGNATURE   L"}"
